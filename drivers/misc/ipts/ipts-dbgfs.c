7e520ac84a5231184feef67d35c1308cf3be63f4
Dorian Stoll <dorian.stoll@tmsp.io>
2019-09-18 22:35:26 +0200

    BACKPORT: Remove IPTS sensor mode APIs
    The previous commit reverted the changes to hid-multitouch that were done by
    the IPTS patch. The original implementation modified hid-multitouch to make the
    single-touch mode work. However, window managers such as GNOME's mutter cannot
    differentiate between two touchscreens on the same panel. This results in broken
    touch input rotation (the single-touch panel gets rotated, even if it is not
    active).
    
    https://github.com/jakeday/linux-surface/issues/549 had some discussion on this
    and multiple fixes. But I think the less changes to unrelated kernel code, the
    better. I don't think that anyone actually uses single-touch mode.
    
    Since single-touch mode was disabled by the previous commit, remove all user-
    space APIs to enable it, since that just sends all touch input to /dev/null
    
    Because IPTS does switch it's mode during initialization, it is not possible to
    remove all references to the sensor mode, at least not without a more in-depth
    review.
    
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>
    
    (cherry-picked from commit 86f95d24802f14977525345e9ea17904d69d589a)

--- drivers/misc/ipts/ipts-dbgfs.c
+++ drivers/misc/ipts/ipts-dbgfs.c
@@ -22,75 +22,10 @@
 #include "ipts-state.h"
 #include "../mei/mei_dev.h"
 
-const char sensor_mode_fmt[] = "sensor mode : %01d\n";
-const char ipts_status_fmt[] = "sensor mode : %01d\nipts state : %01d\n";
+const char ipts_status_fmt[] = "ipts state : %01d\n";
 const char ipts_debug_fmt[] = ">> tdt : fw status : %s\n"
 							  ">> == DB s:%x, c:%x ==\n"
 							  ">> == WQ h:%u, t:%u ==\n";
-
-static ssize_t ipts_dbgfs_mode_read(struct file *fp, char __user *ubuf,
-						size_t cnt, loff_t *ppos)
-{
-	ipts_info_t *ipts = fp->private_data;
-	char mode[80];
-	int len = 0;
-
-	if (cnt < sizeof(sensor_mode_fmt) - 3)
-		return -EINVAL;
-
-	len = scnprintf(mode, 80, sensor_mode_fmt, ipts->sensor_mode);
-	if (len < 0)
-		return -EIO;
-
-	return simple_read_from_buffer(ubuf, cnt, ppos, mode, len);
-}
-
-static ssize_t ipts_dbgfs_mode_write(struct file *fp, const char __user *ubuf,
-						size_t cnt, loff_t *ppos)
-{
-	ipts_info_t *ipts = fp->private_data;
-	ipts_state_t state;
-	int sensor_mode, len;
-	char mode[3];
-
-	if (cnt == 0 || cnt > 3)
-		return -EINVAL;
-
-	state = ipts_get_state(ipts);
-	if (state != IPTS_STA_RAW_DATA_STARTED && state != IPTS_STA_HID_STARTED) {
-		return -EIO;
-	}
-
-	len = cnt;
-	if (copy_from_user(mode, ubuf, len))
-		return -EFAULT;
-
-	while(len > 0 && (isspace(mode[len-1]) || mode[len-1] == '\n'))
-		len--;
-	mode[len] = '\0';
-
-	if (sscanf(mode, "%d", &sensor_mode) != 1)
-		return -EINVAL;
-
-	if (sensor_mode != TOUCH_SENSOR_MODE_RAW_DATA &&
-					sensor_mode != TOUCH_SENSOR_MODE_HID) {
-		return -EINVAL;
-	}
-
-	if (sensor_mode == ipts->sensor_mode)
-		return 0;
-
-	ipts_switch_sensor_mode(ipts, sensor_mode);
-
-	return cnt;
-}
-
-static const struct file_operations ipts_mode_dbgfs_fops = {
-        .open = simple_open,
-        .read = ipts_dbgfs_mode_read,
-        .write = ipts_dbgfs_mode_write,
-        .llseek = generic_file_llseek,
-};
 
 static ssize_t ipts_dbgfs_status_read(struct file *fp, char __user *ubuf,
 						size_t cnt, loff_t *ppos)
@@ -102,8 +37,7 @@ static ssize_t ipts_dbgfs_status_read(struct file *fp, char __user *ubuf,
 	if (cnt < sizeof(ipts_status_fmt) - 3)
 		return -EINVAL;
 
-	len = scnprintf(status, 256, ipts_status_fmt, ipts->sensor_mode,
-						     ipts->state);
+	len = scnprintf(status, 256, ipts_status_fmt, ipts->state);
 	if (len < 0)
 		return -EIO;
 
@@ -298,13 +232,6 @@ int ipts_dbgfs_register(ipts_info_t* ipts, const char *name)
 	dir = debugfs_create_dir(name, NULL);
 	if (!dir)
 		return -ENOMEM;
-
-        f = debugfs_create_file("mode", S_IRUSR | S_IWUSR, dir,
-                                ipts, &ipts_mode_dbgfs_fops);
-        if (!f) {
-                ipts_err(ipts, "debugfs mode creation failed\n");
-                goto err;
-        }
 
         f = debugfs_create_file("status", S_IRUSR, dir,
                                 ipts, &ipts_status_dbgfs_fops);
