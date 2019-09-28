d75a5e5b06928f9e845e3f98544f6461fad24090
Maximilian Luz <luzmaximilian@gmail.com>
2019-09-28 17:59:13 +0200

    USB: Modify lpm check for sdcard-reader

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4172,7 +4172,8 @@ void usb_enable_lpm(struct usb_device *udev)
 	if (!udev || !udev->parent ||
 			udev->speed < USB_SPEED_SUPER ||
 			!udev->lpm_capable ||
-			udev->state < USB_STATE_DEFAULT)
+			udev->state < USB_STATE_DEFAULT ||
+			!udev->bos || !udev->bos->ss_cap)
 		return;
 
 	udev->lpm_disable_count--;
