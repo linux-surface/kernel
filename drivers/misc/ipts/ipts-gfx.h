24f97ef03edaf1ee28d7a5bc57d9ff92f6eb294e
Dorian Stoll <dorian.stoll@tmsp.io>
2019-10-27 23:46:36 +0100

    ipts: Cleanup ipts-gfx to pass checkpatch
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- drivers/misc/ipts/ipts-gfx.h
+++ drivers/misc/ipts/ipts-gfx.h
@@ -1,24 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Intel Precise Touch & Stylus gpu wrapper
- * Copyright (c) 2016, Intel Corporation.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
+ * Intel Precise Touch & Stylus
+ * Copyright (c) 2016 Intel Corporation
  *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
  */
-
 
 #ifndef _IPTS_GFX_H_
 #define _IPTS_GFX_H_
 
-int ipts_open_gpu(ipts_info_t *ipts);
-void ipts_close_gpu(ipts_info_t *ipts);
-intel_ipts_mapbuffer_t *ipts_map_buffer(ipts_info_t *ipts, u32 size, u32 flags);
-void ipts_unmap_buffer(ipts_info_t *ipts, intel_ipts_mapbuffer_t *buf);
+#include <linux/ipts-gfx.h>
+
+#include "ipts.h"
+
+int ipts_open_gpu(struct ipts_info *ipts);
+void ipts_close_gpu(struct ipts_info *ipts);
+
+struct ipts_mapbuffer *ipts_map_buffer(struct ipts_info *ipts,
+		u32 size, u32 flags);
+
+void ipts_unmap_buffer(struct ipts_info *ipts,
+		struct ipts_mapbuffer *buf);
 
 #endif // _IPTS_GFX_H_
