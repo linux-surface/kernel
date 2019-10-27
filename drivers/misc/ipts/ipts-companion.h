9f88893b6f653effad2cb22b0aa6982d6f45627c
Dorian Stoll <dorian.stoll@tmsp.io>
2019-10-28 00:49:00 +0100

    BACKPORT: ipts: Cleanup ipts-companion to pass checkpatch
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>
    
    (cherry-picked from commit f63f7587848627f085af0cadd700a8bb17e19623)

--- drivers/misc/ipts/ipts-companion.h
+++ drivers/misc/ipts/ipts-companion.h
@@ -1,3 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ *
+ * Intel Precise Touch & Stylus
+ * Copyright (c) 2016 Intel Corporation
+ *
+ */
+
 #ifndef _IPTS_COMPANION_H_
 #define _IPTS_COMPANION_H_
 
@@ -7,9 +15,11 @@
 #include "ipts.h"
 
 bool ipts_companion_available(void);
+
 int ipts_request_firmware(const struct firmware **fw, const char *name,
 		struct device *device);
-int ipts_request_firmware_config(ipts_info_t *ipts,
-		ipts_bin_fw_list_t **firmware_config);
+
+int ipts_request_firmware_config(struct ipts_info *ipts,
+		struct ipts_bin_fw_list **firmware_config);
 
 #endif // _IPTS_COMPANION_H_
