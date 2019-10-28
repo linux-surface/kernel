863de5488572d775ea015de2fb026f809b4c0988
Dorian Stoll <dorian.stoll@tmsp.io>
2019-10-28 10:00:44 +0100

    ipts: Cleanup ipts-kernel to pass checkpatch
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- drivers/misc/ipts/ipts-kernel.h
+++ drivers/misc/ipts/ipts-kernel.h
@@ -1,23 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  *
- * Intel Precise Touch & Stylus Linux driver
- * Copyright (c) 2016, Intel Corporation.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * Intel Precise Touch & Stylus
+ * Copyright (c) 2016 Intel Corporation
  *
  */
 
-#ifndef _ITPS_GFX_H
-#define _ITPS_GFX_H
+#ifndef _IPTS_KERNEL_H_
+#define _IPTS_KERNEL_H_
 
-int ipts_init_kernels(ipts_info_t *ipts);
-void ipts_release_kernels(ipts_info_t *ipts);
+#include "ipts.h"
 
-#endif
+int ipts_init_kernels(struct ipts_info *ipts);
+void ipts_release_kernels(struct ipts_info *ipts);
+
+#endif // _IPTS_KERNEL_H_
