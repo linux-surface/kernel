5ab0b86c78aec7f46682d2f1c50bc361fd1b0255
kitakar5525 <34676735+kitakar5525@users.noreply.github.com>
2020-01-16 11:44:27 +0900

    Revert "USB: Modify lpm check for sdcard-reader"
    This reverts commit 218ad8f547866ac8abee892594be1759f2aa4b58.
    
    (corresponds to commit 810a01920dd9f11e896dd9d5caface2a182946a6)

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4164,8 +4164,7 @@ void usb_enable_lpm(struct usb_device *udev)
 	if (!udev || !udev->parent ||
 			udev->speed < USB_SPEED_SUPER ||
 			!udev->lpm_capable ||
-			udev->state < USB_STATE_DEFAULT ||
-			!udev->bos || !udev->bos->ss_cap)
+			udev->state < USB_STATE_DEFAULT)
 		return;
 
 	udev->lpm_disable_count--;
