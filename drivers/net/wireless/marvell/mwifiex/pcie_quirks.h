300fd6232caf7ca7bc5158d1744550432127d0ea
Tsuchiya Yuto <kitakar@gmail.com>
2020-09-29 17:32:22 +0900

    mwifiex: pcie: add reset_wsid quirk for Surface 3
    This commit adds reset_wsid quirk and uses this quirk for Surface 3 on
    card reset.
    
    To reset mwifiex on Surface 3, it seems that calling the _DSM method
    exists in \_SB.WSID [1] device is required.
    
    On Surface 3, calling the _DSM method removes/re-probes the card by
    itself. So, need to place the reset function before performing FLR and
    skip performing any other reset-related works.
    
    Note that Surface Pro 3 also has the WSID device [2], but it seems to need
    more work. This commit only supports Surface 3 yet.
    
    [1] https://github.com/linux-surface/acpidumps/blob/05cba925f3a515f222acb5b3551a032ddde958fe/surface_3/dsdt.dsl#L11947-L12011
    [2] https://github.com/linux-surface/acpidumps/blob/05cba925f3a515f222acb5b3551a032ddde958fe/surface_pro_3/dsdt.dsl#L12164-L12216
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    Patchset: mwifiex

--- drivers/net/wireless/marvell/mwifiex/pcie_quirks.h
+++ drivers/net/wireless/marvell/mwifiex/pcie_quirks.h
@@ -19,5 +19,11 @@
 
 #define QUIRK_FW_RST_D3COLD	BIT(0)
 
+/* Surface 3 and Surface Pro 3 have the same _DSM method but need to
+ * be handled differently. Currently, only S3 is supported.
+ */
+#define QUIRK_FW_RST_WSID_S3	BIT(1)
+
 void mwifiex_initialize_quirks(struct pcie_service_card *card);
 int mwifiex_pcie_reset_d3cold_quirk(struct pci_dev *pdev);
+int mwifiex_pcie_reset_wsid_quirk(struct pci_dev *pdev);
