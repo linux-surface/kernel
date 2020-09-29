6d8f2fa840634998f2baf1a9ab17dbc175e80d54
Tsuchiya Yuto <kitakar@gmail.com>
2020-09-29 17:25:22 +0900

    mwifiex: pcie: add reset_d3cold quirk for Surface gen4+ devices
    To reset mwifiex on Surface gen4+ (Pro 4 or later gen) devices, it
    seems that putting the wifi device into D3cold is required according
    to errata.inf file on Windows installation (Windows/INF/errata.inf).
    
    This patch adds a function that performs power-cycle (put into D3cold
    then D0) and call the function at the end of reset_prepare().
    
    Note: Need to also reset the parent device (bridge) of wifi on SB1;
    it might be because the bridge of wifi always reports it's in D3hot.
    When I tried to reset only the wifi device (not touching parent), it gave
    the following error and the reset failed:
    
        acpi device:4b: Cannot transition to power state D0 for parent in D3hot
        mwifiex_pcie 0000:03:00.0: can't change power state from D3cold to D0 (config space inaccessible)
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    Patchset: wifi

--- drivers/net/wireless/marvell/mwifiex/pcie.c
+++ drivers/net/wireless/marvell/mwifiex/pcie.c
@@ -529,6 +529,13 @@ static void mwifiex_pcie_reset_prepare(struct pci_dev *pdev)
 	mwifiex_shutdown_sw(adapter);
 	clear_bit(MWIFIEX_IFACE_WORK_DEVICE_DUMP, &card->work_flags);
 	clear_bit(MWIFIEX_IFACE_WORK_CARD_RESET, &card->work_flags);
+
+	/* For Surface gen4+ devices, we need to put wifi into D3cold right
+	 * before performing FLR
+	 */
+	if (card->quirks & QUIRK_FW_RST_D3COLD)
+		mwifiex_pcie_reset_d3cold_quirk(pdev);
+
 	mwifiex_dbg(adapter, INFO, "%s, successful\n", __func__);
 
 	card->pci_reset_ongoing = true;
