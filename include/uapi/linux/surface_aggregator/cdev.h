49527e2b342c26c717a66a5358af61e4d6488272
Maximilian Luz <luzmaximilian@gmail.com>
2020-10-01 18:49:55 +0200

    misc: surface_sam: Fix SPDX identifier for uapi header

--- include/uapi/linux/surface_aggregator/cdev.h
+++ include/uapi/linux/surface_aggregator/cdev.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-License-Identifier: GPL-2.0-or-later WITH Linux-syscall-note */
 /*
  * Surface System Aggregator Module (SSAM) user-space EC interface.
  *
