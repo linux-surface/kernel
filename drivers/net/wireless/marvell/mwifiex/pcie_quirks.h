825e8be099ed16cea5da7a6b21a2d7b63556cf8b
Tsuchiya Yuto <kitakar@gmail.com>
2020-10-04 00:11:49 +0900

    mwifiex: pcie: disable bridge_d3 for Surface gen4+
    Currently, mwifiex fw will crash after suspend on recent kernel series.
    On Windows, it seems that the root port of wifi will never enter D3 state
    (stay on D0 state). And on Linux, disabling the D3 state for the
    bridge fixes fw crashing after suspend.
    
    This commit disables the D3 state of root port on driver initialization
    and fixes fw crashing after suspend.
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    Patchset: mwifiex

--- drivers/net/wireless/marvell/mwifiex/pcie_quirks.h
+++ drivers/net/wireless/marvell/mwifiex/pcie_quirks.h
@@ -5,6 +5,7 @@
 
 #define QUIRK_FW_RST_D3COLD	BIT(0)
 #define QUIRK_DO_FLR_ON_BRIDGE	BIT(1)
+#define QUIRK_NO_BRIDGE_D3	BIT(2)
 
 void mwifiex_initialize_quirks(struct pcie_service_card *card);
 int mwifiex_pcie_reset_d3cold_quirk(struct pci_dev *pdev);
