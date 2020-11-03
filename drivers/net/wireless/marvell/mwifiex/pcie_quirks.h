ce9b11dd660f9e7509570f821bd4fa527ee5ce7a
Jonas Dreßler <verdre@v0yd.nl>
2020-11-03 13:28:04 +0100

    mwifiex: Add quirk resetting the PCI bridge on MS Surface devices
    The most recent firmware of the 88W8897 card reports a hardcoded LTR
    value to the system during initialization, probably as an (unsuccessful)
    attempt of the developers to fix firmware crashes. This LTR value
    prevents most of the Microsoft Surface devices from entering deep
    powersaving states (either platform C-State 10 or S0ix state), because
    the exit latency of that state would be higher than what the card can
    tolerate.
    
    Turns out the card works just the same (including the firmware crashes)
    no matter if that hardcoded LTR value is reported or not, so it's kind
    of useless and only prevents us from saving power.
    
    To get rid of those hardcoded LTR reports, it's possible to reset the
    PCI bridge device after initializing the cards firmware. I'm not exactly
    sure why that works, maybe the power management subsystem of the PCH
    resets its stored LTR values when doing a function level reset of the
    bridge device. Doing the reset once after starting the wifi firmware
    works very well, probably because the firmware only reports that LTR
    value a single time during firmware startup.
    
    Patchset: mwifiex

--- drivers/net/wireless/marvell/mwifiex/pcie_quirks.h
+++ drivers/net/wireless/marvell/mwifiex/pcie_quirks.h
@@ -4,6 +4,7 @@
 #include "pcie.h"
 
 #define QUIRK_FW_RST_D3COLD	BIT(0)
+#define QUIRK_DO_FLR_ON_BRIDGE	BIT(1)
 
 void mwifiex_initialize_quirks(struct pcie_service_card *card);
 int mwifiex_pcie_reset_d3cold_quirk(struct pci_dev *pdev);
