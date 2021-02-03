9abc6facbc1bfb14c04dd4adedc54b885ce7bb45
Maximilian Luz <luzmaximilian@gmail.com>
2021-02-04 00:22:11 +0100

    misc: surface_sam: Update based on module
    Update Surface System Aggregator Module driver based on module.
    
    Changes:
     - Various fixes.
    
    Links:
     - https://github.com/linux-surface/surface-aggregator-module/commit/e73516d1d21fb9fe2956b770d73bdf2490001938

--- drivers/misc/surface_aggregator/ssh_request_layer.c
+++ drivers/misc/surface_aggregator/ssh_request_layer.c
@@ -1004,9 +1004,8 @@ int ssh_request_init(struct ssh_request *rqst, enum ssam_request_flags flags,
  *
  * Initializes the given request transport layer and associated packet
  * transport layer. Transmitter and receiver threads must be started
- * separately via ssh_rtl_tx_start() and ssh_rtl_rx_start(), after the
- * request-layer has been initialized and the lower-level serial device layer
- * has been set up.
+ * separately via ssh_rtl_start(), after the request-layer has been
+ * initialized and the lower-level serial device layer has been set up.
  *
  * Return: Returns zero on success and a nonzero error code on failure.
  */
@@ -1056,7 +1055,7 @@ void ssh_rtl_destroy(struct ssh_rtl *rtl)
 }
 
 /**
- * ssh_rtl_tx_start() - Start request transmitter and receiver.
+ * ssh_rtl_start() - Start request transmitter and receiver.
  * @rtl: The request transport layer.
  *
  * Return: Returns zero on success, a negative error code on failure.
