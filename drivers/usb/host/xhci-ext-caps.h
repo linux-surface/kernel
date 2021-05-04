383a393d4ad068552f61e3af74aa3dfa08259474
Maximilian Luz <luzmaximilian@gmail.com>
2021-05-04 02:19:39 +0200

    USB: host: xhci: Increase timeout for HC halt
    On some devices (specifically the SC8180x based Surface Pro X with
    QCOM04A6) HC halt / xhci_halt() times out during boot. Manually binding
    the xhci-hcd driver at some point later does not exhibit this behavior.
    To work around this, double XHCI_MAX_HALT_USEC, which also resolves this
    issue.
    
    Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>

--- drivers/usb/host/xhci-ext-caps.h
+++ drivers/usb/host/xhci-ext-caps.h
@@ -7,8 +7,8 @@
  * Author: Sarah Sharp
  * Some code borrowed from the Linux EHCI driver.
  */
-/* Up to 16 ms to halt an HC */
-#define XHCI_MAX_HALT_USEC	(16*1000)
+/* Up to 32 ms to halt an HC */
+#define XHCI_MAX_HALT_USEC	(32*1000)
 /* HC not running - set to 1 when run/stop bit is cleared. */
 #define XHCI_STS_HALT		(1<<0)
 
