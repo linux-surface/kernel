bc1e69fd31bf789cc9c7cb80e9946053f54c5ede
Maximilian Luz <luzmaximilian@gmail.com>
2021-05-11 01:48:48 +0200

    Revert "hack: fix EFI boot"
    This reverts commit 1dfde018f9460cfa497108ced260268c0fdf5a87.
    
    This is not necessary, we can instead use the 'efi=novamap' command line
    parameters on newer kernels to get the device to boot.
    
    Furthermore, this change causes us to not exit the UEFI boot services at
    all, which is probably not a good idea. Note, however, that not exiting
    boot services also seems to have had some unintentional consequences
    that made things easier for us: Specifically, reverting this change
    causes some issues with the UARTs, which in turn causes a hang and
    following hard-reset of the device. We speculate that this might be due
    to some firmware-state that gets erased when exiting boot services.
    
    Unfortunately that means that, until we have diagnosed this, the UART
    driver module (qcom_geni_serial) needs to be blacklisted, which in turn
    prevents SAM and thus the keyboard/touchpad/battery/... from working.

--- drivers/firmware/efi/libstub/fdt.c
+++ drivers/firmware/efi/libstub/fdt.c
@@ -279,7 +279,6 @@ efi_status_t allocate_new_fdt_and_exit_boot(void *handle,
 		efi_err("Unable to allocate memory for new device tree.\n");
 		goto fail;
 	}
-	efi_info("EFI Page allocation OK\n");
 
 	/*
 	 * Now that we have done our final memory allocation (and free)
@@ -288,8 +287,6 @@ efi_status_t allocate_new_fdt_and_exit_boot(void *handle,
 	status = efi_get_memory_map(&map);
 	if (status != EFI_SUCCESS)
 		goto fail_free_new_fdt;
-
-	efi_info("EFI get_memory_map OK\n");
 
 	status = update_fdt((void *)fdt_addr, fdt_size,
 			    (void *)*new_fdt_addr, MAX_FDT_SIZE, cmdline_ptr,
@@ -300,16 +297,12 @@ efi_status_t allocate_new_fdt_and_exit_boot(void *handle,
 		goto fail_free_new_fdt;
 	}
 
-	efi_info("EFI update_fdt OK\n");
-
 	runtime_entry_count		= 0;
 	priv.runtime_map		= runtime_map;
 	priv.runtime_entry_count	= &runtime_entry_count;
 	priv.new_fdt_addr		= (void *)*new_fdt_addr;
 
 	status = efi_exit_boot_services(handle, &map, &priv, exit_boot_func);
-
-	efi_info("EFI exit_boot_services OK\n");
 
 	if (status == EFI_SUCCESS) {
 		efi_set_virtual_address_map_t *svam;
@@ -343,7 +336,6 @@ efi_status_t allocate_new_fdt_and_exit_boot(void *handle,
 					p->virt_addr = 0;
 			}
 		}
-	  efi_info("EFI success\n");
 		return EFI_SUCCESS;
 	}
 
