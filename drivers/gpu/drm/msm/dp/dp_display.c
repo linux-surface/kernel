40ceeabc5eaa24ed5513dab624a21ef36d3b3b77
Bjorn Andersson <bjorn.andersson@linaro.org>
2021-05-21 08:33:04 -0700

    working edp state
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -402,17 +402,18 @@ static int dp_display_usbpd_configure_cb(struct device *dev)
 
 	if (!dev) {
 		DRM_ERROR("invalid dev\n");
-		return -EINVAL;
+		rc = -EINVAL;
+		goto end;
 	}
 
 	dp = container_of(g_dp_display,
 			struct dp_display_private, dp_display);
 	if (!dp) {
 		DRM_ERROR("no driver data found\n");
-		return -ENODEV;
+		rc = -ENODEV;
+		goto end;
 	}
 
-#if 0
 	dp_display_host_init(dp, false);
 
 	/*
@@ -423,19 +424,6 @@ static int dp_display_usbpd_configure_cb(struct device *dev)
 	rc = dp_display_process_hpd_high(dp);
 end:
 	return rc;
-#endif
-
-	dp_add_event(dp, EV_HPD_PLUG_INT, 0, 0);
-	dp_del_event(dp, EV_CONNECT_PENDING_TIMEOUT);
-	dp_add_event(dp, EV_IRQ_HPD_INT, 0, 0);
-
-	/* DP controller isr */
-	dp_ctrl_isr(dp->ctrl);
-
-	/* DP aux isr */
-	dp_aux_isr(dp->aux);
-
-	return 0;
 }
 
 static int dp_display_usbpd_disconnect_cb(struct device *dev)
@@ -456,16 +444,10 @@ static int dp_display_usbpd_disconnect_cb(struct device *dev)
 		rc = -ENODEV;
 		return rc;
 	}
-		
-	dp_add_event(dp, EV_HPD_UNPLUG_INT, 0, 0);
 
-	/* DP controller isr */
-	dp_ctrl_isr(dp->ctrl);
+	dp_add_event(dp, EV_USER_NOTIFICATION, false, 0);
 
-	/* DP aux isr */
-	dp_aux_isr(dp->aux);
-
-	return 0;
+	return rc;
 }
 
 static void dp_display_handle_video_request(struct dp_display_private *dp)
