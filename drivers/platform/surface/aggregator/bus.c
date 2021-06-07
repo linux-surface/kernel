b18657a4239bad3b786edb87311b130403a5a1fd
Maximilian Luz <luzmaximilian@gmail.com>
2021-06-08 00:24:47 +0200

    platform/surface: aggregator: Allow devices to be marked as hot-removed
    Some SSAM devices, notably the keyboard cover (keyboard and touchpad) on
    the Surface Pro 8, can be hot-removed. When this occurs, communication
    with the device may fail and time out. This timeout can unnecessarily
    block and slow down device removal and even cause issues when the
    devices are detached and re-attached quickly. Thus, communication should
    generally be avoided once hot-removal is detected.
    
    While we already remove a device as soon as we detect its (hot-)removal,
    the corresponding device driver may still attempt to communicate with
    the device during teardown. This is especially critical as communication
    failure may also extend to disabling of events, which is typically done
    at that stage.
    
    Add a flag to allow marking devices as hot-removed. This can then be
    used during client driver teardown to check if any communication
    attempts should be avoided.
    
    Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>
    Patchset: surface-sam

--- drivers/platform/surface/aggregator/bus.c
+++ drivers/platform/surface/aggregator/bus.c
@@ -390,6 +390,9 @@ void ssam_remove_clients(struct device *dev)
 }
 EXPORT_SYMBOL_GPL(ssam_remove_clients);
 
+
+/* -- Bus registration. ----------------------------------------------------- */
+
 /**
  * ssam_bus_register() - Register and set-up the SSAM client device bus.
  */
