d1dc562c092a27bbe881b43f827b541595651633
Daniel Scally <djrscally@gmail.com>
2022-02-10 23:41:20 +0000

    media: uapi: Add IPU3 packed Y10 format
    Some platforms with an Intel IPU3 have an IR sensor producing 10 bit
    greyscale format data that is transmitted over a CSI-2 bus to a CIO2
    device - this packs the data into 32 bytes per 25 pixels. Detail that
    format.
    
    Signed-off-by: Daniel Scally <djrscally@gmail.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -569,6 +569,7 @@ struct v4l2_pix_format {
 /* Grey bit-packed formats */
 #define V4L2_PIX_FMT_Y10BPACK    v4l2_fourcc('Y', '1', '0', 'B') /* 10  Greyscale bit-packed */
 #define V4L2_PIX_FMT_Y10P    v4l2_fourcc('Y', '1', '0', 'P') /* 10  Greyscale, MIPI RAW10 packed */
+#define V4L2_PIX_FMT_IPU3_Y10		v4l2_fourcc('i', 'p', '3', 'y') /* IPU3 packed 10-bit greyscale */
 
 /* Palette formats */
 #define V4L2_PIX_FMT_PAL8    v4l2_fourcc('P', 'A', 'L', '8') /*  8  8-bit palette */
