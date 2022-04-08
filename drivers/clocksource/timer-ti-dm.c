ff714ee7191b55c26540550cebd4c28589e06de1
Tony Lindgren <tony@atomide.com>
2022-04-08 13:17:15 +0300

    clocksource/drivers/timer-ti-dm: Add compatible for am6 SoCs
    Add compatible for ti,am654-timer to support the timers. For example, am654
    has four timers in the MCU domain and 12 timers in the MAIN domain.
    
    Cc: Keerthy <j-keerthy@ti.com>
    Cc: Nishanth Menon <nm@ti.com>
    Cc: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Link: https://lore.kernel.org/r/20220408101715.43697-4-tony@atomide.com
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/timer-ti-dm.c
+++ drivers/clocksource/timer-ti-dm.c
@@ -1037,6 +1037,10 @@ static const struct dmtimer_platform_data omap3plus_pdata = {
 	.timer_ops = &dmtimer_ops,
 };
 
+static const struct dmtimer_platform_data am6_pdata = {
+	.timer_ops = &dmtimer_ops,
+};
+
 static const struct of_device_id omap_timer_match[] = {
 	{
 		.compatible = "ti,omap2420-timer",
@@ -1064,6 +1068,10 @@ static const struct of_device_id omap_timer_match[] = {
 	{
 		.compatible = "ti,dm816-timer",
 		.data = &omap3plus_pdata,
+	},
+	{
+		.compatible = "ti,am654-timer",
+		.data = &am6_pdata,
 	},
 	{},
 };
