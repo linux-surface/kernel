b8a7547e0ca4fea69687412f5b357565490a9179
Andrew Morton <akpm@linux-foundation.org>
2022-04-29 14:42:48 -0700

    lib-kstrtoxc-add-false-true-support-to-kstrtobool-fix
    undo s/iff/if/, per Matthew
    
    Cc: Jagdish Gediya <jvgediya@linux.ibm.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/kstrtox.c
+++ lib/kstrtox.c
@@ -340,7 +340,7 @@ EXPORT_SYMBOL(kstrtos8);
  * @s: input string
  * @res: result
  *
- * This routine returns 0 if the first character is one of 'YyTt1NnFf0', or
+ * This routine returns 0 iff the first character is one of 'YyTt1NnFf0', or
  * [oO][NnFf] for "on" and "off". Otherwise it will return -EINVAL.  Value
  * pointed to by res is updated upon finding a match.
  */
