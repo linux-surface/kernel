c532469fc002f3739aff10fb7fd9afaef79e0c36
Stephen Rothwell <sfr@canb.auug.org.au>
2022-05-02 20:30:07 +1000

    Revert "vmscan: move initialisation of mapping down"
    This reverts commit 8a6aff9c51c7829437438e0e9943b9a10feb7c77.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1588,11 +1588,12 @@ retry:
 			stat->nr_unqueued_dirty += nr_pages;
 
 		/*
-		 * Treat this page as congested if
+		 * Treat this page as congested if the underlying BDI is or if
 		 * pages are cycling through the LRU so quickly that the
 		 * pages marked for immediate reclaim are making it to the
 		 * end of the LRU a second time.
 		 */
+		mapping = page_mapping(page);
 		if (writeback && PageReclaim(page))
 			stat->nr_congested += nr_pages;
 
@@ -1743,6 +1744,9 @@ retry:
 					if (!add_to_swap(folio))
 						goto activate_locked_split;
 				}
+
+				/* Adding to swap updated mapping */
+				mapping = page_mapping(page);
 			}
 		} else if (PageSwapBacked(page) && PageTransHuge(page)) {
 			/* Split shmem THP */
@@ -1783,7 +1787,6 @@ retry:
 			}
 		}
 
-		mapping = folio_mapping(folio);
 		if (folio_test_dirty(folio)) {
 			/*
 			 * Only kswapd can writeback filesystem folios
