35e7032e74a946fb4068f4170cc41f89b1337949
Maximilian Luz <luzmaximilian@gmail.com>
2022-05-18 03:03:03 +0200

    HID: Add driver for HID-over-SPI transport protocol
    Copied from https://github.com/microsoft/surface-duo-oss-kernel.msm-5..4/tree/surfaceduo2/11/2022.108.8/

---
+++ drivers/hid/spi-hid/trace.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/**
+ * trace.c - SPI HID Trace Support
+ *
+ * Copyright (C) 2020 Microsoft Corporation
+ *
+ * Author: Felipe Balbi <felipe.balbi@microsoft.com>
+ */
+
+#define CREATE_TRACE_POINTS
+#include "spi-hid_trace.h"
