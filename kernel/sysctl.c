acdc07ace871d307fb19550bfcdd47ac1a896e64
Fanjun Kong <bh1scw@gmail.com>
2022-05-22 13:29:33 +0800

    kernel/sysctl.c: Clean up indentation, replace spaces with tab.
    This patch fixes two coding style issues:
    1. Clean up indentation, replace spaces with tab
    2. Add space after ','
    
    Signed-off-by: Fanjun Kong <bh1scw@gmail.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1277,8 +1277,8 @@ int proc_dointvec_jiffies(struct ctl_table *table, int write,
 int proc_dointvec_userhz_jiffies(struct ctl_table *table, int write,
 				 void *buffer, size_t *lenp, loff_t *ppos)
 {
-    return do_proc_dointvec(table,write,buffer,lenp,ppos,
-		    	    do_proc_dointvec_userhz_jiffies_conv,NULL);
+	return do_proc_dointvec(table, write, buffer, lenp, ppos,
+				do_proc_dointvec_userhz_jiffies_conv, NULL);
 }
 
 /**
