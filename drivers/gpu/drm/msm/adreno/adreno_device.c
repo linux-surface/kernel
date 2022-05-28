cfe867e1ec55c95033ea692730a7d8b2ed92f4fc
Maximilian Luz <luzmaximilian@gmail.com>
2022-05-28 06:33:51 +0200

    Revert "drm: msm: Add 690 gpu to the adreno gpu list"
    This reverts commit 9ce982ca721b796d7c9133c815935c46305f6708.

--- drivers/gpu/drm/msm/adreno/adreno_device.c
+++ drivers/gpu/drm/msm/adreno/adreno_device.c
@@ -339,19 +339,6 @@ static const struct adreno_info gpulist[] = {
 		.init = a6xx_gpu_init,
 		.zapfw = "a640_zap.mdt",
 		.hwcg = a640_hwcg,
-	}, {
-		.rev = ADRENO_REV(6, 9, 0, ANY_ID),
-		.revn = 690,
-		.name = "A690",
-		.fw = {
-			[ADRENO_FW_SQE] = "a660_sqe.fw",
-			[ADRENO_FW_GMU] = "a660_gmu.bin",
-		},
-		.gmem = SZ_2M,
-		.inactive_period = DRM_MSM_INACTIVE_PERIOD,
-		.init = a6xx_gpu_init,
-		.zapfw = "a660_zap.mdt",
-		.hwcg = a660_hwcg,
 	},
 };
 
