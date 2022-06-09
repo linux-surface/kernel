f7870548589a62c952aa71994dfcb03d12765600
Maximilian Luz <luzmaximilian@gmail.com>
2022-06-09 20:26:26 +0200

    Revert "buffer: Convert clean_bdev_aliases() to use filemap_get_folios()"
    This reverts commit 65faddd2cde84db2ca487fc38057a634b852f353.

--- fs/buffer.c
+++ fs/buffer.c
@@ -1604,7 +1604,7 @@ void clean_bdev_aliases(struct block_device *bdev, sector_t block, sector_t len)
 {
 	struct inode *bd_inode = bdev->bd_inode;
 	struct address_space *bd_mapping = bd_inode->i_mapping;
-	struct folio_batch fbatch;
+	struct pagevec pvec;
 	pgoff_t index = block >> (PAGE_SHIFT - bd_inode->i_blkbits);
 	pgoff_t end;
 	int i, count;
@@ -1612,24 +1612,24 @@ void clean_bdev_aliases(struct block_device *bdev, sector_t block, sector_t len)
 	struct buffer_head *head;
 
 	end = (block + len - 1) >> (PAGE_SHIFT - bd_inode->i_blkbits);
-	folio_batch_init(&fbatch);
-	while (filemap_get_folios(bd_mapping, &index, end, &fbatch)) {
-		count = folio_batch_count(&fbatch);
+	pagevec_init(&pvec);
+	while (pagevec_lookup_range(&pvec, bd_mapping, &index, end)) {
+		count = pagevec_count(&pvec);
 		for (i = 0; i < count; i++) {
-			struct folio *folio = fbatch.folios[i];
+			struct page *page = pvec.pages[i];
 
-			if (!folio_buffers(folio))
+			if (!page_has_buffers(page))
 				continue;
 			/*
-			 * We use folio lock instead of bd_mapping->private_lock
+			 * We use page lock instead of bd_mapping->private_lock
 			 * to pin buffers here since we can afford to sleep and
 			 * it scales better than a global spinlock lock.
 			 */
-			folio_lock(folio);
-			/* Recheck when the folio is locked which pins bhs */
-			head = folio_buffers(folio);
-			if (!head)
+			lock_page(page);
+			/* Recheck when the page is locked which pins bhs */
+			if (!page_has_buffers(page))
 				goto unlock_page;
+			head = page_buffers(page);
 			bh = head;
 			do {
 				if (!buffer_mapped(bh) || (bh->b_blocknr < block))
@@ -1643,9 +1643,9 @@ next:
 				bh = bh->b_this_page;
 			} while (bh != head);
 unlock_page:
-			folio_unlock(folio);
+			unlock_page(page);
 		}
-		folio_batch_release(&fbatch);
+		pagevec_release(&pvec);
 		cond_resched();
 		/* End of range already reached? */
 		if (index > end || !index)
