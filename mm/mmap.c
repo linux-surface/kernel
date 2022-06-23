0aaeda44b27a111eb0473a9adc0d635e30bf239e
Yang Yingliang <yangyingliang@huawei.com>
2022-06-23 21:42:37 +0800

    mm/mmap: fix error return code in do_mas_align_munmap()
    Return error code when munmap_sidetree() fails in do_mas_align_munmap().
    
    Link: https://lkml.kernel.org/r/20220623134237.2127440-1-yangyingliang@huawei.com
    Fixes: 81f5504dfb36 ("mm/mmap: change do_mas_align_munmap() to avoid preallocations for sidetree")
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Reviewed-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2473,7 +2473,8 @@ do_mas_align_munmap(struct ma_state *mas, struct vm_area_struct *vma,
 
 			mas_set(mas, end);
 			split = mas_prev(mas, 0);
-			if (munmap_sidetree(split, &mas_detach))
+			error = munmap_sidetree(split, &mas_detach);
+			if (error)
 				goto munmap_sidetree_failed;
 
 			count++;
@@ -2481,7 +2482,8 @@ do_mas_align_munmap(struct ma_state *mas, struct vm_area_struct *vma,
 				vma = split;
 			break;
 		}
-		if (munmap_sidetree(next, &mas_detach))
+		error = munmap_sidetree(next, &mas_detach);
+		if (error)
 			goto munmap_sidetree_failed;
 
 		count++;
