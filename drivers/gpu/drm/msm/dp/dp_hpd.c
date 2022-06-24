denysvitali/surface-pro-x-linux

drm/msm/dp: Add typec_mux implementation
Implement a typec_mux in order to allow a Type-C controller to signal
the connection and attention of DisplayPort to the related USB-C port.

The remains of support for something along this lines was left in
the dp_display as the driver was upstreamed, so these are reused with
minimal modifications necessary.

When operating in this mode, HPD interrupts has still been observed in
the ISR so, in line with the downstream kernel, these are ignored.

Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

drivers/gpu/drm/msm/dp/dp_hpd.c
@@ -7,6 +7,9 @@
 
 #include <linux/slab.h>
 #include <linux/device.h>
+#include <linux/usb/typec_altmode.h>
+#include <linux/usb/typec_dp.h>
+#include <linux/usb/typec_mux.h>
 
 #include "dp_hpd.h"
 
@@ -22,6 +25,8 @@ struct dp_hpd_private {
 	struct device *dev;
 	struct dp_usbpd_cb *dp_cb;
 	struct dp_usbpd dp_usbpd;
+	struct typec_mux *mux;
+	bool connected;
 };
 
 int dp_hpd_connect(struct dp_usbpd *dp_usbpd, bool hpd)
@@ -47,9 +52,45 @@ int dp_hpd_connect(struct dp_usbpd *dp_usbpd, bool hpd)
 	return rc;
 }
 
+static int dp_hpd_mux_set(struct typec_mux *mux, struct typec_mux_state *state)
+{
+	struct dp_hpd_private *dp_hpd = typec_mux_get_drvdata(mux);
+	struct dp_usbpd *usbpd = &dp_hpd->dp_usbpd;
+	struct typec_displayport_data *dp_data = state->data;
+	int pin_assign = 0;
+
+	if (dp_data) {
+		pin_assign = DP_CONF_GET_PIN_ASSIGN(dp_data->conf);
+		usbpd->hpd_high = !!(dp_data->status & DP_STATUS_HPD_STATE);
+		usbpd->hpd_irq = !!(dp_data->status & DP_STATUS_IRQ_HPD);
+		usbpd->multi_func = pin_assign == DP_PIN_ASSIGN_C || DP_PIN_ASSIGN_E;
+	}
+
+	if (!pin_assign) {
+		if (dp_hpd->connected) {
+			dp_hpd->connected = false;
+			dp_hpd->dp_cb->disconnect(dp_hpd->dev);
+		}
+	} else if (!dp_hpd->connected) {
+		dp_hpd->connected = true;
+		dp_hpd->dp_cb->configure(dp_hpd->dev);
+	} else {
+		dp_hpd->dp_cb->attention(dp_hpd->dev);
+	}
+
+	return 0;
+}
+
+static void dp_hpd_unregister_typec_mux(void *data)
+{
+	typec_mux_unregister(data);
+}
+
 struct dp_usbpd *dp_hpd_get(struct device *dev, struct dp_usbpd_cb *cb)
 {
+	struct typec_mux_desc mux_desc = {};
 	struct dp_hpd_private *dp_hpd;
+	int rc;
 
 	if (!cb) {
 		pr_err("invalid cb data\n");
@@ -64,6 +105,19 @@ struct dp_usbpd *dp_hpd_get(struct device *dev, struct dp_usbpd_cb *cb)
 	dp_hpd->dp_cb = cb;
 
 	dp_hpd->dp_usbpd.connect = dp_hpd_connect;
+
+	mux_desc.fwnode = dev->fwnode;
+	mux_desc.set = dp_hpd_mux_set;
+	mux_desc.drvdata = dp_hpd;
+	dp_hpd->mux = typec_mux_register(dev, &mux_desc);
+	if (IS_ERR(dp_hpd->mux)) {
+		dev_err(dev, "unable to register typec mux\n");
+		return ERR_CAST(dp_hpd->mux);
+	}
+
+	rc = devm_add_action_or_reset(dev, dp_hpd_unregister_typec_mux, dp_hpd->mux);
+	if (rc)
+		return ERR_PTR(rc);
 
 	return &dp_hpd->dp_usbpd;
 }

linux-surface/kernel

drivers/gpu/drm/msm/dp/dp_hpd.c
@@ -7,6 +7,9 @@
 
 #include <linux/slab.h>
 #include <linux/device.h>
+#include <linux/usb/typec_altmode.h>
+#include <linux/usb/typec_dp.h>
+#include <linux/usb/typec_mux.h>
 
 #include "dp_hpd.h"
 
@@ -22,6 +25,8 @@ struct dp_hpd_private {
 	struct device *dev;
 	struct dp_usbpd_cb *dp_cb;
 	struct dp_usbpd dp_usbpd;
+	struct typec_mux_dev *mux;
+	bool connected;
 };
 
 int dp_hpd_connect(struct dp_usbpd *dp_usbpd, bool hpd)
@@ -45,9 +50,44 @@ int dp_hpd_connect(struct dp_usbpd *dp_usbpd, bool hpd)
 	return rc;
 }
 
+static int dp_hpd_mux_set(struct typec_mux_dev *mux, struct typec_mux_state *state)
+{
+	struct dp_hpd_private *dp_hpd = typec_mux_get_drvdata(mux);
+	struct dp_usbpd *usbpd = &dp_hpd->dp_usbpd;
+	struct typec_displayport_data *dp_data = state->data;
+	int pin_assign = 0;
+
+	if (dp_data) {
+		pin_assign = DP_CONF_GET_PIN_ASSIGN(dp_data->conf);
+		usbpd->hpd_irq = !!(dp_data->status & DP_STATUS_IRQ_HPD);
+		usbpd->multi_func = pin_assign == DP_PIN_ASSIGN_C || DP_PIN_ASSIGN_E;
+	}
+
+	if (!pin_assign) {
+		if (dp_hpd->connected) {
+			dp_hpd->connected = false;
+			dp_hpd->dp_cb->disconnect(dp_hpd->dev);
+		}
+	} else if (!dp_hpd->connected) {
+		dp_hpd->connected = true;
+		dp_hpd->dp_cb->configure(dp_hpd->dev);
+	} else {
+		dp_hpd->dp_cb->attention(dp_hpd->dev);
+	}
+
+	return 0;
+}
+
+static void dp_hpd_unregister_typec_mux(void *data)
+{
+	typec_mux_unregister(data);
+}
+
 struct dp_usbpd *dp_hpd_get(struct device *dev, struct dp_usbpd_cb *cb)
 {
+	struct typec_mux_desc mux_desc = {};
 	struct dp_hpd_private *dp_hpd;
+	int rc;
 
 	if (!cb) {
 		pr_err("invalid cb data\n");
@@ -62,6 +102,19 @@ struct dp_usbpd *dp_hpd_get(struct device *dev, struct dp_usbpd_cb *cb)
 	dp_hpd->dp_cb = cb;
 
 	dp_hpd->dp_usbpd.connect = dp_hpd_connect;
+
+	mux_desc.fwnode = dev->fwnode;
+	mux_desc.set = dp_hpd_mux_set;
+	mux_desc.drvdata = dp_hpd;
+	dp_hpd->mux = typec_mux_register(dev, &mux_desc);
+	if (IS_ERR(dp_hpd->mux)) {
+		dev_err(dev, "unable to register typec mux\n");
+		return ERR_CAST(dp_hpd->mux);
+	}
+
+	rc = devm_add_action_or_reset(dev, dp_hpd_unregister_typec_mux, dp_hpd->mux);
+	if (rc)
+		return ERR_PTR(rc);
 
 	return &dp_hpd->dp_usbpd;
 }