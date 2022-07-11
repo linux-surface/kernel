5c9d5ff7a3725bad255606098f7760b6e1f3a120
Johan Hovold <johan+linaro@kernel.org>
2022-07-11 08:48:02 +0200

    drm/panel-edp: add AUO B133UAN02.1 panel entry
    Add an eDP panel entry for AUO B133UAN02.1.
    
    Due to lack of documentation, use the delay_200_500_e50 timings like
    some other AUO entries for now.
    
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -1870,6 +1870,7 @@ static const struct panel_delay delay_100_500_e200 = {
  * Sort first by vendor, then by product ID.
  */
 static const struct edp_panel_entry edp_panels[] = {
+	EDP_PANEL_ENTRY('A', 'U', 'O', 0x1e9b, &delay_200_500_e50, "B133UAN02.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x405c, &auo_b116xak01.delay, "B116XAK01"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x615c, &delay_200_500_e50, "B116XAN06.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x8594, &delay_200_500_e50, "B133UAN01.0"),
