c44054259fa4c5934802ddb86a59f8220d1687e0
Maximilian Luz <luzmaximilian@gmail.com>
2022-08-05 03:02:34 +0200

    clk: qcom: gcc-sc8180x: Keep PCIe GDSCs ALWAYS_ON
    This commit is adapted from "clk: qcom: gcc-sc8280xp: Keep PCIe GDSCs
    ALWAYS_ON" by Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>.
    
    Original message:
    
      PCIe GDSCs must be kept ON during suspend, otherwise during resume,
      trying to access the powered regions causes a crash. This should be
      ideally fixed in the GDSC client drivers like PCIe. But there is no
      clean mechanism available atm. So in the meantime, let's keep all the
      populated PCIe GDSCs in ALWAYS_ON state.
    
    Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>

--- drivers/clk/qcom/gcc-sc8180x.c
+++ drivers/clk/qcom/gcc-sc8180x.c
@@ -4200,7 +4200,7 @@ static struct gdsc pcie_0_gdsc = {
 		.name = "pcie_0_gdsc",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
-	.flags = POLL_CFG_GDSCR,
+	.flags = POLL_CFG_GDSCR | ALWAYS_ON,
 };
 
 static struct gdsc ufs_card_gdsc = {
@@ -4227,7 +4227,7 @@ static struct gdsc pcie_1_gdsc = {
 		.name = "pcie_1_gdsc",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
-	.flags = POLL_CFG_GDSCR,
+	.flags = POLL_CFG_GDSCR | ALWAYS_ON,
 };
 
 static struct gdsc pcie_2_gdsc = {
@@ -4236,7 +4236,7 @@ static struct gdsc pcie_2_gdsc = {
 		.name = "pcie_2_gdsc",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
-	.flags = POLL_CFG_GDSCR,
+	.flags = POLL_CFG_GDSCR | ALWAYS_ON,
 };
 
 static struct gdsc ufs_card_2_gdsc = {
@@ -4254,7 +4254,7 @@ static struct gdsc pcie_3_gdsc = {
 		.name = "pcie_3_gdsc",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
-	.flags = POLL_CFG_GDSCR,
+	.flags = POLL_CFG_GDSCR | ALWAYS_ON,
 };
 
 static struct gdsc usb30_mp_gdsc = {
