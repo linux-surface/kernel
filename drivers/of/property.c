a80b3e6fb414242aed15d3908b0a2bafadea1704
Michael Walle <michael@walle.cc>
2022-12-06 21:07:22 +0100

    of: property: make #.*-cells optional for simple props
    Sometimes, future bindings for phandles will get additional arguments.
    Thus the target node of the phandle will need a new #.*-cells property.
    To be backwards compatible, this needs to be optional.
    
    Prepare the DEFINE_SIMPLE_PROPS() to handle the cells name as optional.
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    Tested-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- drivers/of/property.c
+++ drivers/of/property.c
@@ -1256,8 +1256,8 @@ static struct device_node *parse_suffix_prop_cells(struct device_node *np,
 	if (strcmp_suffix(prop_name, suffix))
 		return NULL;
 
-	if (of_parse_phandle_with_args(np, prop_name, cells_name, index,
-				       &sup_args))
+	if (__of_parse_phandle_with_args(np, prop_name, cells_name, 0, index,
+					 &sup_args))
 		return NULL;
 
 	return sup_args.np;
