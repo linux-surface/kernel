b98df1322d063aee5015bf6fc751cf612151183c
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2023-02-06 21:10:45 -0800

    sched/core: Add user_tick as argument to scheduler_tick()
    Differentiate between user and kernel ticks so that the scheduler updates
    the IPC class of the current task during the former.
    
    Cc: Ben Segall <bsegall@google.com>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Ionela Voinescu <ionela.voinescu@arm.com>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Lukasz Luba <lukasz.luba@arm.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Tim C. Chen <tim.c.chen@intel.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: x86@kernel.org
    Cc: linux-pm@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Patchset: intel-thread-director

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5550,7 +5550,7 @@ static inline u64 cpu_resched_latency(struct rq *rq) { return 0; }
  * This function gets called by the timer code, with HZ frequency.
  * We call it with interrupts disabled.
  */
-void scheduler_tick(void)
+void scheduler_tick(bool user_tick)
 {
 	int cpu = smp_processor_id();
 	struct rq *rq = cpu_rq(cpu);
