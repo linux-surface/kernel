9d71a0e968496cc561b8bd6ed0e0f3dfdae9ff2e
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2023-02-06 21:10:46 -0800

    sched/core: Update the IPC class of the current task
    When supported, hardware monitors the instruction stream to classify the
    current task. Hence, at userspace tick, we are ready to read the most
    recent classification result for the current task.
    
    Cc: Ben Segall <bsegall@google.com>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Ionela Voinescu <ionela.voinescu@arm.com>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Lukasz Luba <lukasz.luba@arm.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Tim C. Chen <tim.c.chen@intel.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: x86@kernel.org
    Cc: linux-pm@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Patchset: intel-thread-director

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5562,6 +5562,9 @@ void scheduler_tick(bool user_tick)
 	if (housekeeping_cpu(cpu, HK_TYPE_TICK))
 		arch_scale_freq_tick();
 
+	if (sched_ipcc_enabled() && user_tick)
+		arch_update_ipcc(curr);
+
 	sched_clock_tick();
 
 	rq_lock(rq, &rf);
