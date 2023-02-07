db0b0e36404f9b091b52d5c1798ca3e875bf3728
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2023-02-06 21:10:44 -0800

    sched/core: Initialize the IPC class of a new task
    New tasks shall start life as unclassified. They will be classified by
    hardware when they run.
    
    Cc: Ben Segall <bsegall@google.com>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Ionela Voinescu <ionela.voinescu@arm.com>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Lukasz Luba <lukasz.luba@arm.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Tim C. Chen <tim.c.chen@intel.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: x86@kernel.org
    Cc: linux-pm@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Patchset: intel-thread-director

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4424,6 +4424,9 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.prev_sum_exec_runtime	= 0;
 	p->se.nr_migrations		= 0;
 	p->se.vruntime			= 0;
+#ifdef CONFIG_IPC_CLASSES
+	p->ipcc				= IPC_CLASS_UNCLASSIFIED;
+#endif
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
