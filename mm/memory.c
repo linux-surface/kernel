7c66fb76c6277c887f9b66ba8367f885b1417cf5
Suren Baghdasaryan <surenb@google.com>
2023-02-27 09:36:27 -0800

    mm: introduce per-VMA lock statistics
    Add a new CONFIG_PER_VMA_LOCK_STATS config option to dump extra statistics
    about handling page fault under VMA lock.
    
    Link: https://lkml.kernel.org/r/20230227173632.3292573-29-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -5273,6 +5273,7 @@ retry:
 	/* Check if the VMA got isolated after we found it */
 	if (vma->detached) {
 		vma_end_read(vma);
+		count_vm_vma_lock_event(VMA_LOCK_MISS);
 		/* The area was replaced with another one */
 		goto retry;
 	}
@@ -5281,6 +5282,7 @@ retry:
 	return vma;
 inval:
 	rcu_read_unlock();
+	count_vm_vma_lock_event(VMA_LOCK_ABORT);
 	return NULL;
 }
 #endif /* CONFIG_PER_VMA_LOCK */
