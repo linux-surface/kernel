909f4bd498ac3a9e21ecd6e2be19d04403224532
Yue Zhao <findns94@gmail.com>
2023-03-09 00:25:53 +0800

    mm-memcg-prevent-memoryswappiness-load-store-tearing-v3
    Add [WRITE|READ]_ONCE for all occurrences of memcg->oom_kill_disable,
    memcg->swappiness and memcg->soft_limit
    
    Link: https://lkml.kernel.org/r/20230308162555.14195-3-findns94@gmail.com
    Signed-off-by: Yue Zhao <findns94@gmail.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Tang Yizhou <tangyeechou@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5353,7 +5353,7 @@ mem_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 #endif
 	page_counter_set_high(&memcg->swap, PAGE_COUNTER_MAX);
 	if (parent) {
-		memcg->swappiness = mem_cgroup_swappiness(parent);
+		WRITE_ONCE(memcg->swappiness, mem_cgroup_swappiness(parent));
 		memcg->oom_kill_disable = parent->oom_kill_disable;
 
 		page_counter_init(&memcg->memory, &parent->memory);
