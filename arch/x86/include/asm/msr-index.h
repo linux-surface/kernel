5ea6fa3c0cffed2fdb553f1ea05584c39054b824
Maximilian Luz <luzmaximilian@gmail.com>
2023-04-20 22:37:17 +0200

    Revert "x86/cpufeatures: Add feature bit for HRESET"
    This reverts commit 0fb34724e6d7c2ab82e7e63ddf8458667eb1961c.

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -1098,9 +1098,6 @@
 #define MSR_IA32_HW_FEEDBACK_THREAD_CONFIG 0x17d4
 #define MSR_IA32_HW_FEEDBACK_CHAR	0x17d2
 
-/* Hardware History Reset  */
-#define MSR_IA32_HW_HRESET_ENABLE	0x17da
-
 /* x2APIC locked status */
 #define MSR_IA32_XAPIC_DISABLE_STATUS	0xBD
 #define LEGACY_XAPIC_DISABLED		BIT(0) /*
@@ -1108,4 +1105,5 @@
 						* disabling x2APIC will cause
 						* a #GP
 						*/
+
 #endif /* _ASM_X86_MSR_INDEX_H */
