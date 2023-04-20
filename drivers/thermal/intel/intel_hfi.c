95e956622a6e1d7e4b729afeb3d297940a81aa91
Maximilian Luz <luzmaximilian@gmail.com>
2023-04-20 22:27:12 +0200

    Revert "thermal: intel: hfi: Enable the Intel Thread Director"
    This reverts commit c5f968b18ba95d49d8847edeaf458e0e3014775f.

--- drivers/thermal/intel/intel_hfi.c
+++ drivers/thermal/intel/intel_hfi.c
@@ -50,8 +50,6 @@
 /* Hardware Feedback Interface MSR configuration bits */
 #define HW_FEEDBACK_PTR_VALID_BIT		BIT(0)
 #define HW_FEEDBACK_CONFIG_HFI_ENABLE_BIT	BIT(0)
-#define HW_FEEDBACK_CONFIG_ITD_ENABLE_BIT	BIT(1)
-#define HW_FEEDBACK_THREAD_CONFIG_ENABLE_BIT	BIT(0)
 
 /* CPUID detection and enumeration definitions for HFI */
 
@@ -72,15 +70,6 @@ union cpuid6_edx {
 		u32			table_pages:4;
 		u32			__reserved:4;
 		s32			index:16;
-	} split;
-	u32 full;
-};
-
-union cpuid6_ecx {
-	struct {
-		u32	dont_care0:8;
-		u32	nr_classes:8;
-		u32	dont_care1:16;
 	} split;
 	u32 full;
 };
@@ -509,11 +498,6 @@ void intel_hfi_online(unsigned int cpu)
 
 	init_hfi_cpu_index(info);
 
-	if (cpu_feature_enabled(X86_FEATURE_ITD)) {
-		msr_val = HW_FEEDBACK_THREAD_CONFIG_ENABLE_BIT;
-		wrmsrl(MSR_IA32_HW_FEEDBACK_THREAD_CONFIG, msr_val);
-	}
-
 	/*
 	 * Now check if the HFI instance of the package/die of @cpu has been
 	 * initialized (by checking its header). In such case, all we have to
@@ -569,21 +553,7 @@ void intel_hfi_online(unsigned int cpu)
 	 */
 	rdmsrl(MSR_IA32_HW_FEEDBACK_CONFIG, msr_val);
 	msr_val |= HW_FEEDBACK_CONFIG_HFI_ENABLE_BIT;
-
-	if (cpu_feature_enabled(X86_FEATURE_ITD))
-		msr_val |= HW_FEEDBACK_CONFIG_ITD_ENABLE_BIT;
-
 	wrmsrl(MSR_IA32_HW_FEEDBACK_CONFIG, msr_val);
-
-	/*
-	 * We have all we need to support IPC classes. Task classification is
-	 * now working.
-	 *
-	 * All class scores are zero until after the first HFI update. That is
-	 * OK. The scheduler queries these scores at every load balance.
-	 */
-	if (cpu_feature_enabled(X86_FEATURE_ITD))
-		sched_enable_ipc_classes();
 
 unlock:
 	mutex_unlock(&hfi_instance_lock);
@@ -662,14 +632,8 @@ static __init int hfi_parse_features(void)
 	 */
 	hfi_features.class_stride = nr_capabilities;
 
-	if (cpu_feature_enabled(X86_FEATURE_ITD)) {
-		union cpuid6_ecx ecx;
-
-		ecx.full = cpuid_ecx(CPUID_HFI_LEAF);
-		hfi_features.nr_classes = ecx.split.nr_classes;
-	} else {
-		hfi_features.nr_classes = 1;
-	}
+	/* For now, use only one class of the HFI table */
+	hfi_features.nr_classes = 1;
 
 	/*
 	 * The header contains change indications for each supported feature.
