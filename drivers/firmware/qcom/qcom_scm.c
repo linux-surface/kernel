d7305a670dc937c7b26e9b648cda1ffc9c3ee2ab
Maximilian Luz <luzmaximilian@gmail.com>
2023-05-21 03:42:29 +0200

    wip! firmware: qcom_scm: Add Surface Pro X to list of tested QSEECOM devices
    Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1726,6 +1726,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "lenovo,flex-5g" },
 	{ .compatible = "lenovo,thinkpad-t14s" },
 	{ .compatible = "lenovo,thinkpad-x13s", },
+	{ .compatible = "microsoft,surface-pro-x", },
 	{ .compatible = "qcom,sc8180x-primus" },
 	{ .compatible = "qcom,x1e80100-crd" },
 	{ .compatible = "qcom,x1e80100-qcp" },
