c83bab88ddc1b7a164016ab31290bf5c89dc5511
Maximilian Luz <luzmaximilian@gmail.com>
2023-05-21 03:40:58 +0200

    firmware: Add support for Qualcomm UEFI Secure Application
    On platforms using the Qualcomm UEFI Secure Application (uefisecapp),
    EFI variables cannot be accessed via the standard interface in EFI
    runtime mode. The respective functions return EFI_UNSUPPORTED. On these
    platforms, we instead need to talk to uefisecapp. This commit provides
    support for this and registers the respective efivars operations to
    access EFI variables from the kernel.
    
    Communication with uefisecapp follows the Qualcomm QSEECOM / Secure OS
    conventions via the respective SCM call interface. This is also the
    reason why variable access works normally while boot services are
    active. During this time, said SCM interface is managed by the boot
    services. When calling ExitBootServices(), the ownership is transferred
    to the kernel. Therefore, UEFI must not use that interface itself (as
    multiple parties accessing this interface at the same time may lead to
    complications) and cannot access variables for us.
    
    Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>

--- drivers/firmware/qcom_scm.c
+++ drivers/firmware/qcom_scm.c
@@ -1691,7 +1691,9 @@ static bool qcom_scm_qseecom_device_allowed(void)
 	return match;
 }
 
-static const struct qseecom_app_desc qcom_scm_qseecom_apps[] = {};
+static const struct qseecom_app_desc qcom_scm_qseecom_apps[] = {
+	{ "qcom.tz.uefisecapp", "uefisecapp" },
+};
 
 static int qcom_scm_qseecom_init(void)
 {
