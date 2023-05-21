5c4e9ed207dbe07e0d161f615e49216efe8abed4
Maximilian Luz <luzmaximilian@gmail.com>
2023-05-21 03:42:29 +0200

    wip! firmware: qcom_scm: Add Surface Pro X to list of tested QSEECOM devices
    Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1651,6 +1651,7 @@ EXPORT_SYMBOL_GPL(qcom_scm_qseecom_app_send);
  */
 static const struct of_device_id qcom_scm_qseecom_allowlist[] = {
 	{ .compatible = "lenovo,thinkpad-x13s", },
+	{ .compatible = "microsoft,surface-pro-x", },
 	{ }
 };
 
