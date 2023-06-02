2b2e5c0d817f7347ecf4f04e04c022ee334b4be7
Joel Granados <j.granados@samsung.com>
2023-06-02 13:06:38 +0200

    sysctl: replace child with a flags var
    This is part of the effort to remove the empty element at the end of
    ctl_table structs. "child" was a deprecated elem in this struct and was
    being used to differentiate between two types of ctl_tables: "normal"
    and "permanently emtpy".
    
    What changed?:
    * Replace "child" with a u8 "flag" variable and use it to differentiate
      between types by refactoring the permanently empty helper functions.
    * Remove the "empty child" check from sysctl_check_table
    * Remove count_subheaders function as there is no longer a need to
      calculate how many headers there are for every child
    * Remove the recursive call to unregister_sysctl_table as there is no
      need to traverse down the child tree any longer
    * Add a new SYSCTL_PERM_EMPTY_DIR binary flag
    * Remove the last remanence of child from partport/procfs.c
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -137,7 +137,7 @@ struct ctl_table {
 	void *data;
 	int maxlen;
 	umode_t mode;
-	struct ctl_table *child;	/* Deprecated */
+	u8 flags;
 	proc_handler *proc_handler;	/* Callback for text formatting */
 	struct ctl_table_poll *poll;
 	void *extra1;
@@ -229,7 +229,7 @@ extern int unaligned_enabled;
 extern int unaligned_dump_stack;
 extern int no_unaligned_warning;
 
-extern struct ctl_table sysctl_mount_point[];
+#define SYSCTL_PERM_EMPTY_DIR	(1 << 0)
 
 #else /* CONFIG_SYSCTL */
 
