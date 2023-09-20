dd7bf6c10972716894bf05079a119fd09a95fb54
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2023-09-20 17:26:33 +0800

    landlock: Move and rename layer helpers
    This patch renames and moves landlock_unmask_layers() and
    landlock_init_layer_masks() helpers to ruleset.c to share
    them with Landlock network implementation in following commits.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Link: https://lore.kernel.org/r/20230920092641.832134-6-konstantin.meskhidze@huawei.com
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- security/landlock/ruleset.h
+++ security/landlock/ruleset.h
@@ -266,5 +266,14 @@ landlock_get_fs_access_mask(const struct landlock_ruleset *const ruleset,
 	return landlock_get_raw_fs_access_mask(ruleset, layer_level) |
 	       LANDLOCK_ACCESS_FS_INITIALLY_DENIED;
 }
+bool landlock_unmask_layers(
+	const struct landlock_rule *const rule,
+	const access_mask_t access_request,
+	layer_mask_t (*const layer_masks)[LANDLOCK_NUM_ACCESS_FS]);
+
+access_mask_t landlock_init_layer_masks(
+	const struct landlock_ruleset *const domain,
+	const access_mask_t access_request,
+	layer_mask_t (*const layer_masks)[LANDLOCK_NUM_ACCESS_FS]);
 
 #endif /* _SECURITY_LANDLOCK_RULESET_H */
