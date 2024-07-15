cdc4ad36a871b7ac43fcc6b2891058d332ce60ce
Matthew Wilcox (Oracle) <willy@infradead.org>
2024-07-15 14:24:01 -0400

    fs: Convert aops->write_begin to take a folio
    Convert all callers from working on a page to working on one page
    of a folio (support for working on an entire folio can come later).
    Removes a lot of folio->page->folio conversions.
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -3545,7 +3545,7 @@ reserve_block:
 }
 
 static int f2fs_write_begin(struct file *file, struct address_space *mapping,
-		loff_t pos, unsigned len, struct page **pagep, void **fsdata)
+		loff_t pos, unsigned len, struct folio **foliop, void **fsdata)
 {
 	struct inode *inode = mapping->host;
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
@@ -3577,18 +3577,20 @@ static int f2fs_write_begin(struct file *file, struct address_space *mapping,
 #ifdef CONFIG_F2FS_FS_COMPRESSION
 	if (f2fs_compressed_file(inode)) {
 		int ret;
+		struct page *page;
 
 		*fsdata = NULL;
 
 		if (len == PAGE_SIZE && !(f2fs_is_atomic_file(inode)))
 			goto repeat;
 
-		ret = f2fs_prepare_compress_overwrite(inode, pagep,
+		ret = f2fs_prepare_compress_overwrite(inode, &page,
 							index, fsdata);
 		if (ret < 0) {
 			err = ret;
 			goto fail;
 		} else if (ret) {
+			*foliop = page_folio(page);
 			return 0;
 		}
 	}
@@ -3608,7 +3610,7 @@ repeat:
 
 	/* TODO: cluster can be compressed due to race with .writepage */
 
-	*pagep = &folio->page;
+	*foliop = folio;
 
 	if (f2fs_is_atomic_file(inode))
 		err = prepare_atomic_write_begin(sbi, &folio->page, pos, len,
