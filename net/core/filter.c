feae34c992eb7191862fb1594c704fbbf650fef8
Paul Chaignon <paul.chaignon@gmail.com>
2025-07-22 16:32:32 +0200

    bpf: Reject narrower access to pointer ctx fields
    commit e09299225d5ba3916c91ef70565f7d2187e4cca0 upstream.
    
    The following BPF program, simplified from a syzkaller repro, causes a
    kernel warning:
    
        r0 = *(u8 *)(r1 + 169);
        exit;
    
    With pointer field sk being at offset 168 in __sk_buff. This access is
    detected as a narrower read in bpf_skb_is_valid_access because it
    doesn't match offsetof(struct __sk_buff, sk). It is therefore allowed
    and later proceeds to bpf_convert_ctx_access. Note that for the
    "is_narrower_load" case in the convert_ctx_accesses(), the insn->off
    is aligned, so the cnt may not be 0 because it matches the
    offsetof(struct __sk_buff, sk) in the bpf_convert_ctx_access. However,
    the target_size stays 0 and the verifier errors with a kernel warning:
    
        verifier bug: error during ctx access conversion(1)
    
    This patch fixes that to return a proper "invalid bpf_context access
    off=X size=Y" error on the load instruction.
    
    The same issue affects multiple other fields in context structures that
    allow narrow access. Some other non-affected fields (for sk_msg,
    sk_lookup, and sockopt) were also changed to use bpf_ctx_range_ptr for
    consistency.
    
    Note this syzkaller crash was reported in the "Closes" link below, which
    used to be about a different bug, fixed in
    commit fce7bd8e385a ("bpf/verifier: Handle BPF_LOAD_ACQ instructions
    in insn_def_regno()"). Because syzbot somehow confused the two bugs,
    the new crash and repro didn't get reported to the mailing list.
    
    Fixes: f96da09473b52 ("bpf: simplify narrower ctx access")
    Fixes: 0df1a55afa832 ("bpf: Warn on internal verifier errors")
    Reported-by: syzbot+0ef84a7bdf5301d4cbec@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=0ef84a7bdf5301d4cbec
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Acked-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://patch.msgid.link/3b8dcee67ff4296903351a974ddd9c4dca768b64.1753194596.git.paul.chaignon@gmail.com
    [shung-hsi.yu: offset(struct bpf_sock_ops, skb_hwtstamp) case was
    dropped becasuse it was only added in v6.2 with commit 9bb053490f1a
    ("bpf: Add hwtstamp field for the sockops prog")]
    Signed-off-by: Shung-Hsi Yu <shung-hsi.yu@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/core/filter.c
+++ net/core/filter.c
@@ -8080,7 +8080,7 @@ static bool bpf_skb_is_valid_access(int off, int size, enum bpf_access_type type
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case offsetof(struct __sk_buff, sk):
+	case bpf_ctx_range_ptr(struct __sk_buff, sk):
 		if (type == BPF_WRITE || size != sizeof(__u64))
 			return false;
 		info->reg_type = PTR_TO_SOCK_COMMON_OR_NULL;
@@ -8597,7 +8597,7 @@ static bool sock_addr_is_valid_access(int off, int size,
 				return false;
 		}
 		break;
-	case offsetof(struct bpf_sock_addr, sk):
+	case bpf_ctx_range_ptr(struct bpf_sock_addr, sk):
 		if (type != BPF_READ)
 			return false;
 		if (size != sizeof(__u64))
@@ -8651,17 +8651,17 @@ static bool sock_ops_is_valid_access(int off, int size,
 			if (size != sizeof(__u64))
 				return false;
 			break;
-		case offsetof(struct bpf_sock_ops, sk):
+		case bpf_ctx_range_ptr(struct bpf_sock_ops, sk):
 			if (size != sizeof(__u64))
 				return false;
 			info->reg_type = PTR_TO_SOCKET_OR_NULL;
 			break;
-		case offsetof(struct bpf_sock_ops, skb_data):
+		case bpf_ctx_range_ptr(struct bpf_sock_ops, skb_data):
 			if (size != sizeof(__u64))
 				return false;
 			info->reg_type = PTR_TO_PACKET;
 			break;
-		case offsetof(struct bpf_sock_ops, skb_data_end):
+		case bpf_ctx_range_ptr(struct bpf_sock_ops, skb_data_end):
 			if (size != sizeof(__u64))
 				return false;
 			info->reg_type = PTR_TO_PACKET_END;
@@ -8735,17 +8735,17 @@ static bool sk_msg_is_valid_access(int off, int size,
 		return false;
 
 	switch (off) {
-	case offsetof(struct sk_msg_md, data):
+	case bpf_ctx_range_ptr(struct sk_msg_md, data):
 		info->reg_type = PTR_TO_PACKET;
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case offsetof(struct sk_msg_md, data_end):
+	case bpf_ctx_range_ptr(struct sk_msg_md, data_end):
 		info->reg_type = PTR_TO_PACKET_END;
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case offsetof(struct sk_msg_md, sk):
+	case bpf_ctx_range_ptr(struct sk_msg_md, sk):
 		if (size != sizeof(__u64))
 			return false;
 		info->reg_type = PTR_TO_SOCKET;
@@ -10837,7 +10837,7 @@ static bool sk_lookup_is_valid_access(int off, int size,
 		return false;
 
 	switch (off) {
-	case offsetof(struct bpf_sk_lookup, sk):
+	case bpf_ctx_range_ptr(struct bpf_sk_lookup, sk):
 		info->reg_type = PTR_TO_SOCKET_OR_NULL;
 		return size == sizeof(__u64);
 
