f25ebe5b31578d2a4d4e66c3817ce4e0c9f412b8
Tooraj Taraz <tooraj.taraz@yahoo.com>
2025-12-31 00:35:50 +0100

    Add camera support for Surface Pro 9
    Experimental camera support for the Surface Pro 9.
    
    Link: https://github.com/linux-surface/linux-surface/pull/1867
    Patchset: cameras

--- drivers/media/pci/intel/ipu-bridge.c
+++ drivers/media/pci/intel/ipu-bridge.c
@@ -60,6 +60,10 @@ static const struct ipu_sensor_config ipu_supported_sensors[] = {
 	IPU_SENSOR_CONFIG("INT33BE", 1, 419200000),
 	/* Onsemi MT9M114 */
 	IPU_SENSOR_CONFIG("INT33F0", 1, 384000000),
+	/* Omnivision OV5693 - Surface Pro 9 */
+	IPU_SENSOR_CONFIG("OVTI5693", 1, 419200000),
+	/* Omnivision OV13858 - Surface Pro 9 */
+	IPU_SENSOR_CONFIG("OVTID858", 4, 540000000),
 	/* Omnivision OV2740 */
 	IPU_SENSOR_CONFIG("INT3474", 1, 180000000),
 	/* Omnivision OV5670 */
