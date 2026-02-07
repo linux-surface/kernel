acf7540bcdf36744dc7b56cc3409c9c341159a2e
Max Yuan <maxyuan@google.com>
2026-02-07 14:46:45 -0500

    gve: Correct ethtool rx_dropped calculation
    [ Upstream commit c7db85d579a1dccb624235534508c75fbf2dfe46 ]
    
    The gve driver's "rx_dropped" statistic, exposed via `ethtool -S`,
    incorrectly includes `rx_buf_alloc_fail` counts. These failures
    represent an inability to allocate receive buffers, not true packet
    drops where a received packet is discarded. This misrepresentation can
    lead to inaccurate diagnostics.
    
    This patch rectifies the ethtool "rx_dropped" calculation. It removes
    `rx_buf_alloc_fail` from the total and adds `xdp_tx_errors` and
    `xdp_redirect_errors`, which represent legitimate packet drops within
    the XDP path.
    
    Cc: stable@vger.kernel.org
    Fixes: 433e274b8f7b ("gve: Add stats for gve.")
    Signed-off-by: Max Yuan <maxyuan@google.com>
    Reviewed-by: Jordan Rhee <jordanrhee@google.com>
    Reviewed-by: Joshua Washington <joshwash@google.com>
    Reviewed-by: Matt Olson <maolson@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Link: https://patch.msgid.link/20260202193925.3106272-3-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [ removed rx_buf_alloc_fail from rx_dropped calculation ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/google/gve/gve_ethtool.c
+++ drivers/net/ethernet/google/gve/gve_ethtool.c
@@ -210,8 +210,7 @@ gve_get_ethtool_stats(struct net_device *netdev,
 	data[i++] = rx_bytes;
 	data[i++] = tx_bytes;
 	/* total rx dropped packets */
-	data[i++] = rx_skb_alloc_fail + rx_buf_alloc_fail +
-		    rx_desc_err_dropped_pkt;
+	data[i++] = rx_skb_alloc_fail + rx_desc_err_dropped_pkt;
 	/* Skip tx_dropped */
 	i++;
 
@@ -285,7 +284,6 @@ gve_get_ethtool_stats(struct net_device *netdev,
 			data[i++] = tmp_rx_bytes;
 			/* rx dropped packets */
 			data[i++] = tmp_rx_skb_alloc_fail +
-				tmp_rx_buf_alloc_fail +
 				tmp_rx_desc_err_dropped_pkt;
 			data[i++] = rx->rx_copybreak_pkt;
 			data[i++] = rx->rx_copied_pkt;
